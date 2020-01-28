884901e3e8c9f8485d8448dbce66f10418cd3706
vijaykolar <vijayikolar@gmail.com>
2020-01-28 22:19:48 +0530

    Working on burger builder and ingredients

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -1,12 +1,23 @@
 import React from 'react';
 import Aux from '../../hoc/Aux';
+import Burger from '../../components/Burger/Burger'
 
 
 class BurgerBuilder extends React.Component {
+  
+  state = {
+    ingredients: {
+      salad: 0,
+      bacon: 0,
+      cheese: 0,
+      meat: 0
+    }
+  }
+
   render() {
     return(
       <Aux>
-        <div>Burger</div>
+        <Burger ingredients={this.state.ingredients} />
         <div>Burger Controls</div>
       </Aux>
     )
